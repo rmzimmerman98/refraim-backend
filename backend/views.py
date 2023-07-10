e7958801533f2df3d9a58de5da12c699a679069d
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-10 19:16:35 -0400

    Create user class to retrive all user data

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+class Users(APIView):
+    def get(self, requests):
+        data = User.objects.all() 
+        serializer = UserSerializer(data, many=True)
+        return JsonResponse(serializer.data, safe=False)
