455fe081d7b2e336219eff3d1bbb56a8b4e64e73
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-11 14:49:02 -0400

    Add password validation

--- backend/serializers.py
+++ backend/serializers.py
@@ -8,9 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
     class Meta():
         model = User
-        fields = ['username','password', 'email']
+        fields = ['username','password', 'email', 'first_name', 'last_name']
     
     def validate(self, attributes):
         if User.objects.filter(email = attributes['email']).exists():
@@ -19,7 +21,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             return attributes
     
     def create(self, data):
-        user = User.objects.create(username=data["username"], email=data["email"])
+        user = User.objects.create(username=data["username"], email=data["email"], first_name=data["first_name"], last_name=data["last_name"])
         user.set_password(data["password"])
         user.save()
         return user 
