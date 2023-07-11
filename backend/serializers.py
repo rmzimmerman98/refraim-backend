160dadda73406917ce15c985197d9d4f38e32b37
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-11 13:43:10 -0400

    Add id to user serializer fields

--- backend/serializers.py
+++ backend/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta():
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
