57142a1c12a67fa485f27341a86a77ee88b07ced
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-12 12:42:33 -0400

    Create view to see all conversations of user

--- backend/views.py
+++ backend/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .models import User
-from .serializers import UserSerializer, RegisterSerializer, TokenSerializer
+from .models import User, Conversation
+from .serializers import UserSerializer, RegisterSerializer, TokenSerializer, ConversationSerializer
 from django.http import JsonResponse
 
 class Users(APIView):
     def get(self, requests, id):
         data = User.objects.filter(id=id) 
         serializer = UserSerializer(data, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+class Conversations(APIView):
+    def get(self, requests, id):
+        data = Conversation.objects.filter(user_id=id)
+        serializer = ConversationSerializer(data, many=True)
         return JsonResponse(serializer.data, safe=False)
     
 class TokenView(TokenObtainPairView):
