a78b035b1932aab6d93cc9098a4e58b369382fe1
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-12 17:04:34 -0400

    Add authentication backends to settings

--- refraim/settings.py
+++ refraim/settings.py
@@ -137,3 +137,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
