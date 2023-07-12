38653041d6290938037a7adbee85d1d55a17c9ff
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-12 12:36:45 -0400

    Create conversation model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations')
+    prompt = models.TextField(max_length=300)
+    refraim = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
