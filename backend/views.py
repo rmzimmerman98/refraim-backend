fc06835ba0b8927128355bd44bbd1c214650692d
npereznyc <natalieperez84@gmail.com>
2023-07-13 12:59:03 -0400

    Made changes to gpt prompt in gpt.py

--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User, Conversation
 from .serializers import UserSerializer, RegisterSerializer, TokenSerializer, ConversationSerializer
 from django.http import JsonResponse
 from .gpt import Gpt3
+from rest_framework.permissions import AllowAny
+
 
 class Users(APIView):
     def get(self, requests, id):
@@ -32,10 +36,15 @@ class TokenView(TokenObtainPairView):
     serializer_class = TokenSerializer
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
     def post(self,request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
         else:
             return JsonResponse(serializer.errors)
