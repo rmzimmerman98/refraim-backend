1f92f17f8bd34ad1c0be0ac7fe8be5a88793070d
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-13 22:24:44 -0400

    Add url for conversation show

--- backend/urls.py
+++ backend/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('token/', views.TokenView.as_view(), name="token_obtain_pair"),
     path('allconversations/<int:id>/', views.Conversations.as_view(), name="allconversations"),
     path('allconversations/<int:id>/favorites/', views.Favorites.as_view(), name="favorites"),
+    path('conversation/<int:id>/', views.ConversationShow.as_view(), name="conversationshow"),
     path('googlelogin/', views.GoogleLoginView.as_view(), name="googlelogin"),
 ]
