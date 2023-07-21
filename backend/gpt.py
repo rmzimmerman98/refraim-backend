e3af5ecd70db0cd1c89232e5578a4392bfecbf0a
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-21 16:00:43 -0400

    Update refraim messages in gpt.py

--- backend/gpt.py
+++ backend/gpt.py
@@ -15,16 +15,16 @@ class Gpt3:
             'content': 'I want you to help me reframe negative thoughts.'
             },
             {"role": "user", 
-             "content" : "I'm not good enough"},
+             "content" : "I'm feeling misunderstood in my friendship with Bob. He always cancels on me when we make plans. Maybe I should just let go of him as a friend."},
             {"role": "assistant", 
-             "content" : "It appears that you might be experiencing the cognitive distortion known as 'all-or-nothing thinking' or 'black-and-white thinking.' \n This distortion involves seeing things in extremes, where you perceive yourself as either completely good or completely inadequate. \n Remember, it's important to challenge this distortion with more balanced thinking. \n Here are three positive reframes to consider: \n 1. I have unique strengths and qualities that make me valuable. \n 2. I am constantly growing and learning, and my worth is not defined by perfection. \n 3. I am deserving of love, acceptance, and compassion, just as I am."},
+             "content" : "I understand that you're feeling misunderstood in your friendship with Bob, and it seems like you're experiencing a cognitive distortion called 'all-or-nothing thinking.' By believing that Bob always cancels on you when you make plans, you're perceiving the situation in extreme terms without considering any exceptions or other factors that could be contributing to his cancellations. It might be helpful to have an open conversation with Bob about your feelings to gain a better understanding of what's going on and explore possible solutions together."},
             {
             'role': 'user',
             'content': f'Content is : {content}'
             },
             {
             'role': 'user',
-            'content': f'Take the {content} and identify a cognitive distortion that I might be suffering from. In a compassionate tone, state the cognitive distortion and explain it in simple terms in one sentence. Offer a one-sentence positive reframe.'
+            'content': f'Take the {content} and identify with the emotions and feelings that I might have. In a compassionate tone, state the negative thoughts and explain it in simple terms in one sentence. Offer a one or two sentence positive reframe.'
             }
             
         ]
