b2d1cf9b9243166a1497648fb0f8f71bb20347bd
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-22 09:11:30 -0400

    Add conclusion field to conversation model

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,7 @@ class Conversation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations')
     prompt = models.TextField(max_length=1000)
     refraim = models.TextField(max_length=1000)
+    conclusion = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     is_favorite = models.BooleanField(default=False)
 
